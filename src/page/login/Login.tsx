11d37eba3e99be8390ee6da9e071dffe9bcdffe9
theo550 <theo.gaudin35@gmail.com>
2023-11-21 14:37:16 +0100

    Added style on Login page

--- src/page/login/Login.tsx
+++ src/page/login/Login.tsx
@@ -1,13 +1,37 @@
+import styled from 'styled-components'
 import LoginHeader from '../../components/login/LoginHeader'
 import LoginInput from '../../components/login/LoginInput'
+import background from '../../assets/tarts.jpg'
 
 const Login = () => {
   return (
-    <div>
+    <LoginWrapper>
       <LoginHeader/>
       <LoginInput/>
-    </div>
+    </LoginWrapper>
   )
 }
 
 export default Login
+
+const LoginWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  position: relative;
+
+  background: center url(${background});
+  background-size: cover;
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0,0,0,.5);
+  }
+`;
